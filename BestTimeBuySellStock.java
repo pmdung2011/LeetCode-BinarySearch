1ff306e7f137cda7da02c914a58ccc844091a5dc
pmdung2011@gmail.com <pmdung2011@gmail.com>
2021-03-07 18:58:14 -0600

    *

--- BestTimeBuySellStock.java
+++ BestTimeBuySellStock.java
@@ -1,7 +1,3 @@
-package HackerRank;
-
-import com.sun.security.jgss.GSSUtil;
-
 //Leetcode
 public class BestTimeBuySellStock {
     public static int maxProfit(int[] prices) {
@@ -14,8 +10,30 @@ public class BestTimeBuySellStock {
         return maxProfit;
     }
 
+    public static int solve(int[] prices){
+        if(prices == null) return 0;
+        int bought = prices[0]; //Assume we bought the stock in the first day
+        int maxProfit = 0;
+
+        //Start from the second day
+        for(int i = 1; i < prices.length; i++){
+            //if the price in the next day is higher than we have bought
+            //Then consider selling on that day to get the profit
+            if(prices[i] > bought){
+                maxProfit = Math.max(prices[i] - bought, maxProfit);
+            }
+            //If the price in the next day is lower than we have bought
+            //Consider buy the stock on that day
+            else{
+                bought = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
         int[] stocks = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(stocks));
+        System.out.println(solve(stocks));
     }
 }
--- ThreeSum.java
+++ ThreeSum.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class ThreeSum {
+//Leetcode
+//3Sum
+
+import java.util.*;
+
+/**
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * Notice that the solution set must not contain duplicate triplets.
+ * Example 1:
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ */
+
+/**
+ * The complexity of this solution is O(n2) because we need to iterate over all
+ * the numbers (which is O(n)) and for every selected number
+ * find all pairs so that the sum would be 0 (which is O(n)
+ * using two pointer technique)
+ */
+public class ThreeSum {
+    public static List<List<Integer>> solve(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums.length < 3) return res; //It requires 3 nums at least in the array
+        Arrays.sort(nums); //It takes O(nlogn) to sort but group same num together
+
+        //Looking for X + Y + Z = 0 or X + Y = -Z
+        //which are nums[i], nums[left], nums[right]
+        for (int i = 0; i < nums.length - 2 ; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {continue; }
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right){
+                int currentSum = nums[left] + nums[right];
+                //In case we found the 3 nums
+                if(currentSum == -nums[i]){
+                    res.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
+                    //Skip the same num
+                    while(left < right && nums[left] == nums[left-1]){
+                        left++;
+                    }
+                }
+                //Narrow down the section
+                else if (currentSum > -nums[i]){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
 }
