2ecb1976b8f642d1e0b9c41b56d8eaa254c17d30
Danny Phung <pmdung2011@gmail.com>
2021-03-10 14:48:21 -0600

    *

--- CountPrimes.java
+++ CountPrimes.java
@@ -3,8 +3,24 @@ import java.util.List;
 
 //204. Count Primes
 
-//Wrong solution
+
 public class CountPrimes {
+    /**
+     * algorithm Sieve of Eratosthenes is
+    input: an integer n > 1.
+    output: all prime numbers from 2 through n.
+
+    let A be an array of Boolean values, indexed by integers 2 to n,
+    initially all set to true.
+
+    for i = 2, 3, 4, ..., not exceeding √n do
+        if A[i] is true
+            for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n do
+                A[j] := false
+
+    return all i such that A[i] is true.
+     
+     */
     public int countPrimes2(int n) {
         if (n <= 2)
             return 0;
