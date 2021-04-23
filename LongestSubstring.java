2bf85b157b145818fa6383c97aec7b6f4ab1f952
FuMiYu2011 <pmdung2011@gmail.com>
2021-04-23 00:04:46 -0500

    *

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -2,10 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * For every new character, we look for it in the already visited characters. If the character has already been visited and is part of the current substring with non-repeating characters, we update the start index. Otherwise, we'll continue traversing the string.
- *
+ * For every new character, we look for it in the already visited characters.
+ * If the character has already been visited and is part of the current substring with non-repeating characters,
+ * we update the start index. Otherwise, we'll continue traversing the string.
  * Since we are traversing the string only once, the time complexity will be linear, or O(n).
- *
  * This approach is also known as the sliding window pattern.
  */
 public class LongestSubstring {
