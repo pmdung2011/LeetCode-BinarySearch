f28bfac6ba245e8752af0ebf674c9c39b88e5d15
FuMiYu2011 <pmdung2011@gmail.com>
2021-05-14 00:53:01 -0500

    +Climbing Stairs

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -11,4 +11,19 @@ public class ClimbingStairs {
 
         return dp[n]; //the final result will be the combination of the two previous steps
     }
+
+    public int solve2 (int n){
+        //base case
+        if(n <= 1) return 1;
+        int prev1 = 1; //in case one stair, only one way
+        int prev2 = 2; //two stairs, there are two ways (can climb 1 or 2 steps)
+
+        for(int i = 3; i <= n; i++){
+            int newStep = prev1 + prev2; //the next result is the number of ways of two previous ways
+            //move up the ways
+            prev1 = prev2;
+            prev2 = newStep;
+        }
+        return prev2;
+    }
 }
