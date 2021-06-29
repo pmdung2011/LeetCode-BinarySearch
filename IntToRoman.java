19bc6bb8e2a190940599adc2c3a83408dfef440c
FuMiYu2011 <pmdung2011@gmail.com>
2021-06-29 00:27:15 -0500

    IntToRoman

--- IntToRoman.java
+++ IntToRoman.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class IntToRoman {
+public class IntToRoman {
+
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    public static String solve(int num){
+        StringBuilder sb = new StringBuilder();
+        // Loop through each symbol, stopping if num becomes 0.
+        for (int i = 0; i < values.length && num > 0; i++) {
+            // Repeat while the current symbol still fits into num.
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println(solve(31));
+    }
 }
--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class InvertBinaryTree {
+public class InvertBinaryTree {
+    //Recursive approach
+    //Call invert for left -subtree
+    //Call invert for right-subtree
+    //swap left and right subtrees
+
+    public TreeNode solve(TreeNode root){
+        if(root == null) return root;
+
+        //Invert left and right subtrees
+        TreeNode left = solve(root.left);
+        TreeNode right = solve(root.right);
+
+        //Swap left and right subtrees
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
 }
